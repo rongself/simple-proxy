1273e30dafd5fad0eede50efe193d7fa687520b8
rongself <rongself@qq.com>
2017-06-17 16:46:06 +0800

    add install script

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-if [ ! -f install ]; then
-    echo 'install must be run within its container folder' 1>&2
-    exit 1
-fi
 CURDIR=`pwd`
 OLDGOPATH="$GOPATH"
 export GOPATH="$CURDIR"
 gofmt -w src
-go install server
+go install server \
+&& go install server \
+&& mkdir -p bin/config \
+&& cp ./src/config/client.config.json ./src/config/server.config.json bin/config
 export GOPATH="$OLDGOPATH"
 echo 'finished'
